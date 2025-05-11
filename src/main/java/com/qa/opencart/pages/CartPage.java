dce1dc1db29d39263286c881e3d5c3e14626de38
pritishm <mohantybetun@gmail.com>
2025-05-11 21:12:33 +0530

    changes are done as per PritishTL review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,10 +1,12 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
-	int i = 0;
+	private By cart = By.id("cart");
 
 	public void cart() {
-
+		System.out.println("cart using" + cart);
 	}
 }
