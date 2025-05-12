f8903a251416ac738b1caf28ae686eb35d19c9f8
pritishm <mohantybetun@gmail.com>
2025-05-12 23:09:02 +0530

    deleted files

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++
@@ -1,8 +0,0 @@
-package com.qa.opencart.pages;
-
-public class DemoPage {
-
-	public void f2() {
-		System.out.println("f2 feature");
-	}
-}
--- src/main/java/com/qa/opencart/pages/Rest.java
+++
@@ -1,7 +0,0 @@
-package com.qa.opencart.pages;
-
-class Rest {
-	public static void main(String[] args) {
-		System.out.println("Rest");
-	}
-}
