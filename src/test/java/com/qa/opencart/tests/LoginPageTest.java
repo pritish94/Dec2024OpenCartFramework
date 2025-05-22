05589458da0ac62b6b8615d4d75168af5f59c28e
pritishm <mohantybetun@gmail.com>
2025-05-22 12:42:12 +0530

    updated selenium grid remote server URL

--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -23,44 +23,44 @@ import io.qameta.allure.Story;
 @Story("US 101: implement login page for open cart application")
 public class LoginPageTest extends BaseTest {
 
-	@Description("checking open cart login page title...")
-	@Severity(SeverityLevel.MINOR)
-	@Owner("Pritish")
-	@Test(description = "checking login page title")
-	public void loginPageTitleTest() {
-		String actTitle = loginPage.getLoginPageTitle();
-		ChainTestListener.log("checking the login page title: " + actTitle);
-		Assert.assertEquals(actTitle, LOGIN_PAGE_TITLE);
-	}
+    @Description("checking open cart login page title...")
+    @Severity(SeverityLevel.MINOR)
+    @Owner("Pritish")
+    @Test(description = "checking login page title")
+    public void loginPageTitleTest() {
+	String actTitle = loginPage.getLoginPageTitle();
+	ChainTestListener.log("checking the login page title: " + actTitle);
+	Assert.assertEquals(actTitle, LOGIN_PAGE_TITLE);
+    }
 
-	@Description("checking open cart login page url...")
-	@Severity(SeverityLevel.NORMAL)
-	@Owner("Pritish")
-	@Test(description = "checking login page url")
-	public void loginPageURLTest() {
-		String actURL = loginPage.getLoginPageURL();
-		Assert.assertTrue(actURL.contains(LOGIN_PAGE_FRACTION_URL));
-	}
+    @Description("checking open cart login page url...")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("Pritish")
+    @Test(description = "checking login page url")
+    public void loginPageURLTest() {
+	String actURL = loginPage.getLoginPageURL();
+	Assert.assertTrue(actURL.contains(LOGIN_PAGE_FRACTION_URL));
+    }
 
-	@Description("checking open cart login page has forgot pwd link...")
-	@Severity(SeverityLevel.CRITICAL)
-	@Owner("Pritish")
-	@Test(description = "forgotPwdLinkExistTest")
-	public void forgotPwdLinkExistTest() {
-		Assert.assertTrue(loginPage.isForgotPwdLinkExist());
-	}
+    @Description("checking open cart login page has forgot pwd link...")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("Pritish")
+    @Test(description = "forgotPwdLinkExistTest")
+    public void forgotPwdLinkExistTest() {
+	Assert.assertTrue(loginPage.isForgotPwdLinkExist());
+    }
 
-	@Description("checuser is able to login with valid user credentials...")
-	@Severity(SeverityLevel.BLOCKER)
-	@Owner("Pritish")
-	@Test(priority = Short.MAX_VALUE, description = "login with valid credentials")
-	public void doLoginTest() {
-		accPage = loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
-		Assert.assertEquals(accPage.getAccPageTitle(), HOME_PAGE_TITLE);
-	}
+    @Description("check user is able to login with valid user credentials...")
+    @Severity(SeverityLevel.BLOCKER)
+    @Owner("Pritish")
+    @Test(priority = Short.MAX_VALUE, description = "login with valid credentials")
+    public void doLoginTest() {
+	accPage = loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
+	Assert.assertEquals(accPage.getAccPageTitle(), HOME_PAGE_TITLE);
+    }
 
-	@Test(enabled = false, description = "WIP -- forgot pwd check")
-	public void forgotPwd() {
-		System.out.println("forgot pwd");
-	}
+    @Test(enabled = false, description = "WIP -- forgot pwd check")
+    public void forgotPwd() {
+	System.out.println("forgot pwd");
+    }
 }
--- src/test/java/com/qa/opencart/tests/ProductInfoPageTest.java
+++ src/test/java/com/qa/opencart/tests/ProductInfoPageTest.java
@@ -15,66 +15,68 @@ import com.qa.opencart.utils.ExcelUtil;
 
 public class ProductInfoPageTest extends BaseTest {
 
-	@BeforeClass
-	public void productInfoSetup() {
-		accPage = loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
-	}
+    @BeforeClass
+    public void productInfoSetup() {
+	accPage = loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
+    }
 
-	// AAA --- Arrange Act Assert(1)
+    // search()-failed
 
-	@DataProvider
-	public Object[][] getProductTestData() {
-		return new Object[][] { { "macbook", "MacBook Pro" }, { "macbook", "MacBook Air" }, { "imac", "iMac" },
-				{ "samsung", "Samsung SyncMaster 941BW" }, { "samsung", "Samsung Galaxy Tab 10.1" } };
-	}
+    // AAA --- Arrange Act Assert(1)
 
-	@Test(dataProvider = "getProductTestData")
-	public void productHeaderTest(String searchKey, String productName) {
-		searchResultsPage = accPage.doSearch(searchKey);
-		productInfoPage = searchResultsPage.selectProduct(productName);
-		String actHeader = productInfoPage.getProductHeader();
-		Assert.assertEquals(actHeader, productName);
-	}
+    @DataProvider
+    public Object[][] getProductTestData() {
+	return new Object[][] { { "macbook", "MacBook Pro" }, { "macbook", "MacBook Air" }, { "imac", "iMac" },
+		{ "samsung", "Samsung SyncMaster 941BW" }, { "samsung", "Samsung Galaxy Tab 10.1" } };
+    }
 
-	@DataProvider
-	public Object[][] getProductImagesTestData() {
-		return new Object[][] { { "macbook", "MacBook Pro", 4 }, { "macbook", "MacBook Air", 4 }, { "imac", "iMac", 3 },
-				{ "samsung", "Samsung SyncMaster 941BW", 1 }, { "samsung", "Samsung Galaxy Tab 10.1", 7 } };
-	}
+    @Test(dataProvider = "getProductTestData")
+    public void productHeaderTest(String searchKey, String productName) {
+	searchResultsPage = accPage.doSearch(searchKey);
+	productInfoPage = searchResultsPage.selectProduct(productName);
+	String actHeader = productInfoPage.getProductHeader();
+	Assert.assertEquals(actHeader, productName);
+    }
 
-	@DataProvider
-	public Object[][] getProductSheetData() {
-		return ExcelUtil.getTestData(AppConstants.PRODUCT_SHEET_NAME);
-	}
+    @DataProvider
+    public Object[][] getProductImagesTestData() {
+	return new Object[][] { { "macbook", "MacBook Pro", 4 }, { "macbook", "MacBook Air", 4 }, { "imac", "iMac", 3 },
+		{ "samsung", "Samsung SyncMaster 941BW", 1 }, { "samsung", "Samsung Galaxy Tab 10.1", 7 } };
+    }
 
-	@DataProvider
-	public Object[][] getProductCSVData() {
-		return CSVUtil.csvData("product");
-	}
+    @DataProvider
+    public Object[][] getProductSheetData() {
+	return ExcelUtil.getTestData(AppConstants.PRODUCT_SHEET_NAME);
+    }
 
-	@Test(dataProvider = "getProductCSVData")
-	public void productImageCountTest(String searchKey, String productName, String expectedImageCount) {
-		searchResultsPage = accPage.doSearch(searchKey);
-		productInfoPage = searchResultsPage.selectProduct(productName);
-		int actImageCount = productInfoPage.getProductImagesCount();
-		Assert.assertEquals(String.valueOf(actImageCount), expectedImageCount);
-	}
+    @DataProvider
+    public Object[][] getProductCSVData() {
+	return CSVUtil.csvData("product");
+    }
 
-	@Test
-	public void productInfoTest() {
-		searchResultsPage = accPage.doSearch("macbook");
-		productInfoPage = searchResultsPage.selectProduct("MacBook Pro");
-		Map<String, String> actualProductdetailsMap = productInfoPage.getProductDetailsMap();
+    @Test(dataProvider = "getProductCSVData")
+    public void productImageCountTest(String searchKey, String productName, String expectedImageCount) {
+	searchResultsPage = accPage.doSearch(searchKey);
+	productInfoPage = searchResultsPage.selectProduct(productName);
+	int actImageCount = productInfoPage.getProductImagesCount();
+	Assert.assertEquals(String.valueOf(actImageCount), expectedImageCount);
+    }
 
-		SoftAssert softassert = new SoftAssert();
+    @Test
+    public void productInfoTest() {
+	searchResultsPage = accPage.doSearch("macbook");
+	productInfoPage = searchResultsPage.selectProduct("MacBook Pro");
+	Map<String, String> actualProductdetailsMap = productInfoPage.getProductDetailsMap();
 
-		softassert.assertEquals(actualProductdetailsMap.get("Brand"), "Apple");
-		softassert.assertEquals(actualProductdetailsMap.get("Product Code"), "Product 18");
-		softassert.assertEquals(actualProductdetailsMap.get("Availability"), "Out Of Stock");
-		softassert.assertEquals(actualProductdetailsMap.get("productprice"), "$2,000.00");
-		softassert.assertEquals(actualProductdetailsMap.get("extaxprice"), "$2,000.00");
+	SoftAssert softassert = new SoftAssert();
 
-		softassert.assertAll();
-	}
+	softassert.assertEquals(actualProductdetailsMap.get("Brand"), "Apple");
+	softassert.assertEquals(actualProductdetailsMap.get("Product Code"), "Product 18");
+	softassert.assertEquals(actualProductdetailsMap.get("Availability"), "Out Of Stock");
+	softassert.assertEquals(actualProductdetailsMap.get("productprice"), "$2,000.00");
+	softassert.assertEquals(actualProductdetailsMap.get("extaxprice"), "$2,000.00");
+
+	softassert.assertAll();
+    }
 
 }
