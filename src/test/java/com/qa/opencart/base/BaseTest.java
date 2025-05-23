ed1ec951791781f1b0e17770271ee99357eb3a27
pritishm <mohantybetun@gmail.com>
2025-05-23 23:11:19 +0530

    addded dockerized selenoid code

--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -23,50 +23,52 @@ import io.qameta.allure.Description;
 
 //@Listeners(ChainTestListener.class)
 public class BaseTest {
-	private WebDriver driver;
+    private WebDriver driver;
 
-	private DriverFactory df;
-	protected Properties prop;
+    private DriverFactory df;
+    protected Properties prop;
 
-	protected LoginPage loginPage;
-	protected AccountsPage accPage;
-	protected SearchResultsPage searchResultsPage;
-	protected ProductInfoPage productInfoPage;
-	protected RegisterPage registerPage;
+    protected LoginPage loginPage;
+    protected AccountsPage accPage;
+    protected SearchResultsPage searchResultsPage;
+    protected ProductInfoPage productInfoPage;
+    protected RegisterPage registerPage;
 
-	private static final Logger log = LogManager.getLogger(BaseTest.class);
+    private static final Logger log = LogManager.getLogger(BaseTest.class);
 
-	@Description("init the driver and properties")
-	@Parameters({ "browser" })
-	@BeforeTest
-	public void setup(String browserName) {
-		df = new DriverFactory();
-		prop = df.initProp();
+    @Description("init the driver and properties")
+    @Parameters({ "browser", "browserversion", "testname" })
+    @BeforeTest
+    public void setup(String browserName, String browserVersion, String testname) {
+	df = new DriverFactory();
+	prop = df.initProp();
 
-		// browserName is passed from .xml file
-		if (browserName != null) {
-			prop.setProperty("browser", browserName);
-		}
-
-		driver = df.initDriver(prop);
-		loginPage = new LoginPage(driver);
+	// browserName is passed from .xml file
+	if (browserName != null) {
+	    prop.setProperty("browser", browserName);
+	    prop.setProperty("browserversion", browserVersion);
+	    prop.setProperty("testname", testname);
 	}
 
-	@AfterMethod // will be running after each @test method
-	public void attachScreenshot(ITestResult result) {
-		if (!result.isSuccess()) {// only for failure test cases -- true
-			log.info("---screenshot is taken---");
-			ChainTestListener.embed(DriverFactory.getScreenshotFile(), "image/png");
-		}
+	driver = df.initDriver(prop);
+	loginPage = new LoginPage(driver);
+    }
+
+    @AfterMethod // will be running after each @test method
+    public void attachScreenshot(ITestResult result) {
+	if (!result.isSuccess()) {// only for failure test cases -- true
+	    log.info("---screenshot is taken---");
+	    ChainTestListener.embed(DriverFactory.getScreenshotFile(), "image/png");
+	}
 
 //		ChainTestListener.embed(DriverFactory.getScreenshotFile(), "image/png");
 
-	}
+    }
 
-	@Description("closing the browser..")
-	@AfterTest
-	public void tearDown() {
-		driver.quit();
-		log.info("----closing the browser----");
-	}
+    @Description("closing the browser..")
+    @AfterTest
+    public void tearDown() {
+	driver.quit();
+	log.info("----closing the browser----");
+    }
 }
